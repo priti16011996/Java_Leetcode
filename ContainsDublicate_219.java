7ce9b8973d20723b4fdf378da96923742c9ea951
Priti <priti16maurya@gmail.com>
2026-02-07 23:56:25 +0530

    Contains Dublicate

---
+++ ContainsDublicate_219.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ContainsDublicate_219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> hs = new HashSet<>();
+        for(int i=0; i< nums.length; i++)
+        {
+            if(!hs.add(nums[i]))
+            {
+                return true;
+            }
+            if(hs.size()>k)
+            {
+                hs.remove(nums[i-k]);
+            }
+        }
+        return false;
+    }
+}
+
