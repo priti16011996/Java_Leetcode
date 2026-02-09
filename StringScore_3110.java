5b591f66bd7e3a35e9f85c711adfeb62df56598b
Priti <priti16maurya@gmail.com>
2026-02-09 23:53:08 +0530

    String Score java

---
+++ StringScore_3110.java
@@ -0,0 +1,10 @@
+public class StringScore_3110 {
+    public int scoreOfString(String s) {
+        int score =0;
+        for(int i=1; i<s.length(); i++)
+        {
+            score += Math.abs(s.charAt(i)-s.charAt(i-1));
+        }
+        return score;
+    }
+}
